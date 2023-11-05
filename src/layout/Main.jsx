a219573f59e3bb4ac3a4a0ee8e909e95e3daa106
Nazrul Islam <dobon.islam@icloud.com>
2023-11-04 22:42:28 -0400

    navbar added' git add . clear

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../pages/Shared/Footer";
+import Navbar from "../pages/Shared/Navbar";
 
 
 const Main = () => {
     return (
         <div>
-            <h1>This is home</h1>
+            <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 
 import {
- 
+
   RouterProvider,
 } from "react-router-dom";
 import router from './routes/Routes.jsx';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+  <div className='max-w-[1200px] mx-auto'>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>,
+  </div>
 )
