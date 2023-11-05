f9bf3fb92429904df7e669bc14925fbef68066e3
Nazrul Islam <dobon.islam@icloud.com>
2023-11-05 00:07:24 -0400

    banner section added

---
+++ src/pages/Home/Home/Banner.jsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+
+
+const Banner = () => {
+    return (
+        <div className="hero h-[500px] mt-6 " style={{ backgroundImage: 'url(https://i.ibb.co/KKYJVyy/banner.png)' }}>
+            <div className="hero-overlay bg-opacity-60"></div>
+            <div className="hero-content text-center text-neutral-content">
+
+            </div>
+            <div className="  pt-[360px] text-blue-300 ">
+                <p className="text-2xl text-center">StoryStream Company</p>
+                <p className="text-4xl text-center">{`"BlogCraft: Nurturing Your Online Voice"`}</p>
+               <Link to = "/login">
+               <div className="text-center ">
+                    <button className="btn no-underline btn-link text-white border-spacing-0.5 ">start here</button>
+                </div>
+                </Link>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
