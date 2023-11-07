da9bd7ad9de4629af51814123cfbe45c0c479f4f
Nazrul Islam <dobon.islam@icloud.com>
2023-11-07 13:32:20 -0500

    Add blog from added

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,8 +13,10 @@ module.exports = {
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
+      "react/prop-types" : "off",
       'warn',
       { allowConstantExport: true },
+      
     ],
   },
 }
--- src/pages/AddBLog/AddBlog.jsx
+++ src/pages/AddBLog/AddBlog.jsx
@@ -3,7 +3,75 @@
 const AddBlog = () => {
     return (
         <div>
-            <h1>This is Add Blog page</h1>
+            <div className=" bg-[#c9c3ee] p-24 ">
+                <h2 className="text-3xl font-bold text-center">ADD BLOGS</h2>
+                <form>
+                    {/* form row */}
+                    <div className="md:flex mb-6 ">
+                        <div className="form-control w-full ">
+                            <label className="label">
+                                <span className="label-text">Title</span>
+                            </label>
+                            <label className="input-group">
+                                <input type="text" name="title" placeholder="title" className="input input-bordered w-full" />
+                            </label>
+                        </div>
+                       
+                    </div>
+                    {/* form row */}
+                    <div className="md:flex mb-6">
+                    <div className="form-control w-full" >
+                            <select className="select select-bordered w-full">
+                                <option disabled selected>Category</option>
+                                <option>Beauty Blog</option>
+                                <option>Edu. Blog</option>
+                                <option>Fashion Blog</option>
+                                <option>Food Blog</option>
+                                <option>Gaming Blog</option>
+                                <option>Travel Blog</option>
+                            </select>
+
+                        </div>
+                        
+                    </div>
+                    {/* form row */}
+                    <div className="md:flex mb-6">
+                        <div className="form-control w-full">
+                            <label className="label">
+                                <span className="label-text">Short Description</span>
+                            </label>
+                            <label className="input-group">
+                                <input type="text" name="price" placeholder="short description" className="input input-bordered w-full" />
+                            </label>
+                        </div>
+                        
+                    </div>
+                    <div className="md:flex mb-6">
+                        <div className="form-control w-full">
+                            <label className="label">
+                                <span className="label-text">Long Description</span>
+                            </label>
+                            <label className="input-group">
+                            <textarea placeholder="description" className="textarea textarea-bordered textarea-md w-full" ></textarea>
+                            </label>
+                        </div>
+                        
+                    </div>
+                    {/* form photo url row */}
+                    <div className=" mb-6">
+                        <div className="form-control w-full">
+                            <label className="label">
+                                <span className="label-text">Photo URL</span>
+                            </label>
+                            <label className="input-group">
+                                <input type="text" name="image" placeholder="photo url" className="input input-bordered w-full" />
+                            </label>
+                        </div>
+                    </div>
+                    <input type="submit" value="Add Blog" name="submit" className="btn btn-block border-none bg-blue-400 text-white" />
+                </form>
+
+            </div>
         </div>
     );
 };
